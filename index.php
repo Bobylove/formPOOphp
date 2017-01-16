d162d3fa58648d6ae8c64694b3938aa31591ce49
Fred <delon.frederic.pro.job@gmail.com>
2017-01-16 16:33:59 +0100

    step 6 clear

--- index.php
+++ index.php
@@ -3,34 +3,50 @@
 	<meta charset="UTF-8">
 	<title>formPOO</title>
 </head>
-<h1>form</h1>
+<h1>Form POO</h1>
 <br>
+<?php if (empty($_POST)) { ?>
 <form action="user.php" method="post">
+	<label for="genre">Genre</label>
+	<select name="genre" id="genre">
+		<option value="homme">Mr</option>
+		<option value="femme">Mme</option>
+	</select>
+	<br>
+	<br>
 	<label for="nom">Nom</label>
 	<input name="nom" id="nom" type="text">
-	<br>
+	<br><br>
 	<label for="prenom">Prenom</label>
 	<input name="prenom" id="prenom" type="text">
-	<br>	
+	<br><br>	
 	<input type="submit" value="CLICK HERE">
 </form>
 <?php 
+}else{
+	
+}
 class FormInfos {
 	private $_name = "";
 	private $_prenom = "";
+	private $_genre = "";
 	
+	function __construct($_nom,$_prename,$_gender){
 
-	public function __construct($arg){
-		return $this->_name.$this->_prenom = $arg;
+		$this->_name = $_nom;
+		$this->_prenom = $_prename;
+		$this->_genre = $_gender;
+
+	}
+	public function setGenre(){
+		return $this->_genre;
 	}
 
 	public function setPrenom(){
-		$this->_prenom = $_POST["prenom"];
 		return  $this->_prenom;
 	}
 	public function setName(){
-		$this->_nom = $_POST["nom"];
-		return $this->_nom;
+		return $this->_name;
 
 	}
 }
--- user.php
+++ user.php
@@ -1,7 +1,8 @@
 <?php 
 require "index.php";
 
-$newInfos = new FormInfos();
-echo $newInfos->setPrenom();
-echo  $newInfos->setName();
+$newInfos = new FormInfos($_POST["nom"],$_POST["prenom"],$_POST["genre"]);
+echo "Genre : " .  $newInfos->setGenre() . "<br>";
+echo "prenom :  " . $newInfos->setPrenom() . "<br>" ;
+echo "nom :  " . $newInfos->setName() ;
 ?>
